ee4822b3e2c83b2913fab386b99dfaf11e366287
Pedro Nunez Araujo <pedrinais@hotmail.com>
2018-03-09 02:30:54 -0800

    Modifica metas sin las notas de creditos

--- metasventa/trigger.sql
+++ metasventa/trigger.sql
@@ -1,5 +1,5 @@
-drop view metasventa_vendedoresreport;
 
+drop table  metasventa_metasvendedores;                      
 
 create or replace function trg_metas_vendedores_stat() 
 returns trigger as $trg_metas_vendedores_stat$
@@ -10,20 +10,21 @@ begin
   execute 'truncate table metasventa_vendedoresreport';
 
   for cc in  select u.id, 
-                    u.login as name,
-                    m.ano as ano,
-                    m.mes as mes,
+                    r.name as name,
+                    m.ano::text as ano,
+                    m.mes::text as mes,
                     m.monto as montometa,
-                    sum(i.amount_untaxed) as montofacturado,                              
-                    sum((i.amount_untaxed/m.monto) * 100) as logro
+                    sum(i.amount_untaxed_signed) as montofacturado,                              
+                    sum((i.amount_untaxed_signed/m.monto) * 100) as logro
                from metasventa_metasvendedores as m                        
                     join res_users u on u.id = m.vendedor
+                    join res_partner r on r.id = u.partner_id
                     join account_invoice i on m.vendedor = i.user_id
                 and u.id  = i.user_id
                 and i.state in ('paid','open')
                 and m.ano = extract(year from  i.date_invoice)::int
                 and m.mes = extract(month from  i.date_invoice)::int
-              group by u.id,u.login,m.ano,m.mes,m.monto  loop
+              group by u.id,r.name,m.ano,m.mes,m.monto  loop
 			  
 			 insert into metasventa_vendedoresreport
 			            (name,ano,mes,montometa, montofacturado,logro)
@@ -37,8 +38,6 @@ begin
 $trg_metas_vendedores_stat$ language plpgsql;
 
 
-create trigger trg_metas_vendedores_stat after insert or update or delete on account_invoice
-  for each row execute procedure trg_metas_vendedores_stat();
 
 
 
